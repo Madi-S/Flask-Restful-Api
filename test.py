37036d04f7cfe0001bd90ed23190895b3eec948b
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-19 18:35:18 +0600

    requests tests

---
+++ test.py
@@ -0,0 +1,25 @@
+import requests
+
+BASE = 'http://127.0.0.1:5000/'
+
+r = requests.get(BASE + 'hello')
+print(r.json())
+
+r = requests.post(BASE + 'hello', data={'token': '54612315', 'age': 25})
+print(r.json())
+
+r = requests.get(BASE + 'health/62/73')
+print(r.json())
+
+
+r = requests.put(BASE + 'test/54', {'name': 'Madi', 'age': 17, 'gender': 'male'})
+print(r.json())
+
+r = requests.get(BASE + 'test/54')
+print(r.json())
+
+r = requests.delete(BASE + 'test/54')
+print(r.json())
+
+r = requests.get(BASE + 'test/54')
+print(r.json())
