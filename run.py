b43df623261f9945876669a1a0f4c2b683098360
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-19 21:29:41 +0600

    resouces modifed

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import app, api
-from views import HelloWorld, Health, Test, Football
+from views import HelloWorld, Health, Test, Faker
 
 
 if __name__ == '__main__':
     api.add_resource(HelloWorld, '/hello')
     api.add_resource(Health, '/health/<int:age>/<int:weight>')
     api.add_resource(Test, '/test/<int:user_id>')
-    api.add_resource(Football, '/football/<string:team_name>')
+    api.add_resource(Faker, '/faker/<string:api_key>')
     app.run()
