882be92070a7b287a2b9e54bd074b417eb0bebd0
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-19 18:34:26 +0600

    db uri changed

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Flask-Restful-Api\flask.db'
+    SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Flask-Restful-Api\restfulapi.db'
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
