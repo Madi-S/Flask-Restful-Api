7b84d86cecaa61cdbf574b433575aea040563c5d
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-20 13:19:22 +0600

    job methods changed

--- models.py
+++ models.py
@@ -1,48 +1,73 @@
-from app import db
+import rq
+import redis
+
+from app import db, app
 from uuid import uuid4
 from datetime import timedelta
 
 
 
-class JobQueue:
-    __tablename__ = 'job_queue'
-    to_eval = db.Column(db.String(200))
+class Job(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    is_completed = db.Column(db.Boolean, default=False)
+
+    key = db.Column(db.Integer, db.ForeignKey('api_user.key'))
+
+    def __repr__(self):
+        return f'<Job obj #{self.id}: {self.name}, {self.description}>'
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return True
+
+    @staticmethod
+    def create(id, api_key):
+        j = Job(id=id, key=api_key)
+        db.session.add(j)
+        db.session.commit()
+
+
+class APIUser(db.Model):    
     id = db.Column(db.Integer, primary_key=True)
-    api_user_key = db.Column(db.Integer, db.ForeignKey('api_user.key'), nullable=False)
+    api_calls = db.Column(db.Integer, default=0, nullable=False) 
+    api_calls_limit = db.Column(db.Integer, default=5, nullable=False)
+    api_call_interval = db.Column(db.Interval, nullable=False, default=timedelta(hours=12))
+    
+    key = db.Column(db.String, db.ForeignKey('api_key.key'), nullable=False)
+
+    def __repr__(self):
+        return f'<APIUser obj #{self.id}: {self.key}, {self.api_calls}>'
+
+    # def launch_task(self, name, description, *args, **kwargs):
+    #     rq_job = current_app.task_queue.enqueue('app.tasks.' + name, self.id,
+    #                                             *args, **kwargs)
+    #     task = Task(id=rq_job.get_id(), name=name, description=description,
+    #                 user=self)
+    #     db.session.add(task)
+    #     return task
+
+    def flush_api_calls(self):
+        self.api_calls = 0
+        db.session.commit()
+        return True
+
+    @staticmethod
+    def increment_api_calls_by_n(self, n=1):
+        self.api_calls += n
+        db.session.commit()
+        return True
 
 
 class APIKey(db.Model):
-    __tablename__ = 'api_key'
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.Integer, unique=True, default=lambda: uuid4().hex)
-    api_user = db.relationship('api_user', backref='person', uselist=False, lazy=True)
     is_valid = db.Column(db.Boolean, default=True)
+    
+    user = db.relationship('APIUser', backref='key_obj', uselist=False, lazy=True)
 
-
-class APIUser(db.Model):
-    __tablename__ = 'api_user'
-    id = db.Column(db.Integer, primary_key=True)
-    api_calls = db.Column(db.Integer, default=0, nullable=False) 
-    api_calls_limit = db.Column(db.Integer, defalut=5, nullable=False)
-    key = db.Column(db.String, db.ForeignKey('api_key.key'), nullable=False)
-    api_call_interval = db.Column(db.Interval, nullable=False, default=timedelta(hours=12))
-
-    @staticmethod
-    def flush_api_calls(api_user_key):
-        api_user = APIUser.query.filter_by(key=api_user_key).first()
-        if api_user:
-            api_user.api_calls = 0
-            db.session.commit()
-            return True
-
-    @staticmethod
-    def increment_api_calls_by_n(api_user_key, n):
-        api_user = APIUser.query.filter_by(key=api_user_key).first()
-        if api_user:
-            api_user.api_calls += 0
-            db.session.commit()
-            return True
-
+    def __repr__(self):
+        return f'<APIKey obj #{self.id}: {self.key}, {self.is_valid}>'
 
 
 class User(db.Model):
@@ -52,7 +77,6 @@ class User(db.Model):
     middle_name = db.Column(db.String(100))
 
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
-
 
     def __repr__(self):
         return f'<User obj #{self.id}: {self.first_name} {self.last_name} {self.last_name}>'
