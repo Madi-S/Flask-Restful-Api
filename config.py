18b6257358fe61ae45222b8349757b9d579720b2
Madi <72963376+Madi-S@users.noreply.github.com>
2021-03-20 13:19:12 +0600

    redis_url

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from os import environ
 
 
 class Configuration:
@@ -5,3 +6,5 @@ class Configuration:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Flask-Restful-Api\restfulapi.db'
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
+
+    REDIS_URL = 'redis://'          # environ.get('REDIS_URL')
