a1e51874411f39a566de03365f9a9dffb9c0133f
Madi-S <khovansky99@gmail.com>
2021-03-20 14:32:35 +0600

    db uri changed

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from os import environ
+from sys import platform
 
 
 class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Flask-Restful-Api\restfulapi.db'
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
 
-    REDIS_URL = 'redis://'          # environ.get('REDIS_URL')
+    REDIS_URL = 'redis://'                                              # environ.get('REDIS_URL')
