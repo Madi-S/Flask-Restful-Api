ea82ff809ad3c18d296b5c3b85a5140e909f07ad
Madi-S <khovansky99@gmail.com>
2021-03-21 12:22:26 +0600

    testing rq worker successful

---
+++ test/worker_module.py
@@ -0,0 +1,15 @@
+from func_module import count_words_at_url, say_hello
+from redis import Redis
+from rq import Queue
+from datetime import timedelta, datetime
+
+q = Queue(connection=Redis())
+
+result = q.enqueue(count_words_at_url, 'http://nvie.com')
+print(result)
+
+# Schedule job to run at 9:15, October 10th
+job = q.enqueue_at(datetime(2019, 10, 8, 9, 15), say_hello)
+
+# Schedule job to be run in 10 seconds
+job = q.enqueue_in(timedelta(seconds=10), say_hello)
