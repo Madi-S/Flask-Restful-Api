a1cabc929fd6918b884c47c1d2848fc886701c1e
Madi-S <khovansky99@gmail.com>
2021-03-21 12:12:28 +0600

    redis url

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 from os import environ
-from sys import platform
 
 
 class Configuration:
@@ -8,4 +7,4 @@ class Configuration:
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
 
-    REDIS_URL = 'redis://'                                              # environ.get('REDIS_URL')
+    # REDIS_URL = 'redis://'                                              # environ.get('REDIS_URL')
