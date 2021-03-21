7e7a2a94c65c8a1fe19b9885f0dcbe640d51ca58
Madi-S <khovansky99@gmail.com>
2021-03-21 15:07:50 +0600

    redis url removed

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ class Configuration:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
-
-    # REDIS_URL = 'redis://'                                              # environ.get('REDIS_URL')
