976a5fe57fa639bf3ced6039352d9756e72b710f
Madi-S <khovansky99@gmail.com>
2021-03-22 20:35:18 +0600

    response logging, automatically inserting api key in request

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+from models import APIUser, logger
 
 BASE = 'http://127.0.0.1:5000/'
 
@@ -24,6 +25,9 @@ BASE = 'http://127.0.0.1:5000/'
 # r = requests.get(BASE + 'test/54')
 # print(r.json())
 
+key = APIUser.query.get(1).api_user_key
 
-r = requests.get(BASE + '/faker/2f4b92e03f374723b1b64ad7d2798bd3')
-print(r)
+r = requests.get(BASE + f'/faker/{key}')
+logger.debug('Response: %s', r)
+logger.debug('JSON: %s', r.json())
+logger.debug('Text: %s', r.text)
