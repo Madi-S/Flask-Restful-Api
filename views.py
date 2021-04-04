2ccd0409950c6c2d59f8b2754267c7ccc77b007c
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-04 19:28:57 +0600

    basic views

---
+++ views.py
@@ -0,0 +1,47 @@
+from app import app, admin, logger, api
+from flask_admin.contrib.sqla import ModelView
+from flask import render_template, redirect, url_for, request, session, flash, send_from_directory
+
+from functools import wraps
+from models import APIUser, APIKey, db
+
+import os
+
+
+class AdminModelView(ModelView):
+    # def is_accessible(self):
+    #     return 'admin' in session
+    pass
+
+
+def login_required(f):
+    @wraps
+    def inner(*args, **kwargs):
+        user_key = session.get('user_key')
+        if user_key and APIUser.query.filter_by(user_key=user_key).first():
+            return f(*args, **kwargs)
+
+        return redirect(url_for('index'))
+
+    return inner
+
+
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/logout')
+def logout():
+    session.pop('user_key', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'images/favicon.ico', mimetype='image/vnd.microsoft.icon')
