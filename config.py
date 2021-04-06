6e1d805f5ffad6f450d941c235df6a3c6db6e5d9
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-06 20:11:36 +0600

    googlemaps key removed

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Configuration:
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
 
-    GOOGLEMAPS_KEY = '8JZ7i18MjFuM35dJHq70n3Hx4'
 
     CACHE_TYPE = 'SimpleCache'
     CACHE_DEFAULT_TIMEOUT = 300
