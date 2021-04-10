4734a079fa93ae29ae72c545715fec2ab76df507
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-10 13:32:50 +0600

    windows db uri

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 from os import environ
+from sys import platform
 
 
 class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'super_secret_hasehd_encrypted_key_to_protect_my_sophisticated_web_application_from_various_hackers_on_a_daily_basis'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
+
+    if platform.startswith('w'):
+        # For Windows
+        SQLALCHEMY_DATABASE_URI = r'sqlite:///C:\Users\khova\Desktop\Python\Code\Flask-Restful-Api\restful.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/retful.db'                  # 'mysql+mysqlconnector://root:1234@localhost/restful'
 
 
     CACHE_TYPE = 'SimpleCache'
